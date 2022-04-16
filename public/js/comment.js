83272faeeaac792500af1a1dc5deb6ce89b35295
Baeonetta <victor.m.kennedy@gmail.com>
2022-04-15 19:35:43 -0500

    corrected handlebars landing page and added dashboard

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,3 +1,5 @@
+
+
 const commentFormHandler = async (event) => {
   event.preventDefault();
 
@@ -20,6 +22,28 @@ const commentFormHandler = async (event) => {
   }
 };
 
+const postForm = async (event) => {
+  event.preventDefault();
+  const comment = $('#comment_text').value.trim()
+  if (comment) {
+    await fetch('/dashboard', {
+      method: 'POST',
+      body: JSON.stringify({
+        comment_id,
+        comment_text
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    document.location.reload();
+  }
+};
+
+postbtn.addEventListener('submit', postForm)
+
 document
   .querySelector('#new-comment-form')
   .addEventListener('submit', commentFormHandler);
+
+
